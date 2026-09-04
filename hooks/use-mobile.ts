cf24ae04156ac71448cbbddf0d3bf679b91d2bde
Hilton Martins <hiltonmbr@gmail.com>
2026-09-04 16:01:14 -0300

    style(hooks): format use-mobile.ts with prettier

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -9,7 +9,7 @@ export function useIsMobile() {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
     const onChange = () => {
       // eslint-disable-next-line react-hooks/set-state-in-effect
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
     mql.addEventListener("change", onChange)
     onChange()
